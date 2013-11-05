cf67e569b3f07a4e744737fdc5cdeddaf98d31ed
Ola Lindberg <ola@olalindberg.com>
2013-11-05 11:49:04 +0100

    Just comments

--- osx/backup-godaddy.sh
+++ osx/backup-godaddy.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-#todo: dump DB
-
 rsync -PzrlptgD --del --delete-excluded -r --rsync-path=~/bin/rsync godaddy:~/ /Users/Ola/Backup/godaddy/
 
+#---
+
+#todo: dump DB
+
+#mysqldump -u root -p --all-databases > all_dbs.sql
+
+#för att få in dom: mysql -u root -p < all_dbs.sql
+
 #todo: backup wordpress.com blogs
+#todo: rsync retries https://gist.github.com/iangreenleaf/279849 ?
